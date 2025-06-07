6635e1b4e239534307bb61a120bd0cb3139c0dd3
Sameer Sangpal <66773934+SameerSangpal@users.noreply.github.com>
2025-06-07 16:34:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function stringChop(str, size) {
   // your code here
+	if(str === null) return [];
+	const chunks = [];
+	for(let i = 0; i < str.length; i += size){
+		chunks.push(str.slice(i, i + size));
+	}
+	return chunks;
 }
 
+/* const result = stringChop("12345", 2);
+console.log(JSON.stringify(result)); */
 // Do not change the code below
 const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+const size = parseInt(prompt("Enter Chunk Size."));
+alert(JSON.stringify(stringChop(str, size)));
